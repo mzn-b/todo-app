80834dcf799fe0232fb70fc828dfe7340e6fbde3
mzn-b <medomedso300@gmail.com>
2025-01-16 17:25:32 +0100

    update namespace names

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -49,7 +49,7 @@ export default function NavBar () {
                   
                 </Menu> 
                 <Typography variant="h6">
-                TODO App Rudi 2
+                Mazen El-shaarawi
               </Typography>
             </Toolbar>
           </AppBar>
